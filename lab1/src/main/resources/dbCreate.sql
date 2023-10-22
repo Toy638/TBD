9558863893b87cb29f7ce13c213b988debe626c8
FS <franco.salvo@usach.cl>
2023-10-21 23:57:21 -0300

    fix: dbCreate without POSTGIS

--- lab1/src/main/resources/dbCreate.sql
+++ lab1/src/main/resources/dbCreate.sql
@@ -16,7 +16,7 @@ CREATE TABLE "institucion" (
                                "fecha" DATE,
                                "direccion" varchar(255),
                                "telefono" varchar(20),
-                               "id_usuario" int
+                               "id_usuario" integer unique
 );
 
 CREATE TABLE "voluntario" (
@@ -25,7 +25,7 @@ CREATE TABLE "voluntario" (
                               "apellido" varchar(255),
                               "telefono" varchar(20),
                               "direccion" varchar(255),
-                              "id_usuario" int
+                              "id_usuario" integer unique
 );
 
 CREATE TABLE "emergencia" (
@@ -35,39 +35,39 @@ CREATE TABLE "emergencia" (
                               "direccion" varchar(255),
                               "fecha" DATE,
                               "active" BOOLEAN,
-                              "id_institucion" int
+                              "id_institucion" integer unique
 );
 
 CREATE TABLE "tarea" (
                          "id_tarea" serial PRIMARY KEY,
                          "asunto_tarea" varchar(255),
                          "estado_tarea" BOOLEAN,
-                         "id_emergencia" int
+                         "id_emergencia" integer unique
 );
 
 CREATE TABLE "eme_habilidad" (
                                  "id_eme_habilidad" serial PRIMARY KEY,
-                                 "id_habilidad" int,
-                                 "id_emergencia" int
+                                 "id_habilidad" integer,
+                                 "id_emergencia" integer unique
 );
 
 CREATE TABLE "ranking" (
                            "id_ranking" serial PRIMARY KEY,
-                           "id_tarea" int,
-                           "id_voluntario" int,
-                           "puntaje" int
+                           "id_tarea" integer,
+                           "id_voluntario" integer,
+                           "puntaje" integer
 );
 
 CREATE TABLE "tarea_habilidad" (
                                    "id_tarea_habilidad" serial PRIMARY KEY,
-                                   "id_tarea" int,
-                                   "id_habilidad" int
+                                   "id_tarea" integer unique,
+                                   "id_habilidad" integer unique
 );
 
 CREATE TABLE "vol_habilidad" (
                                  "id_vol_habilidad" serial,
-                                 "id_voluntario" int,
-                                 "id_habilidad" int
+                                 "id_voluntario" integer,
+                                 "id_habilidad" integer unique
 );
 
 ALTER TABLE "usuario" ADD FOREIGN KEY ("id_usuario") REFERENCES "institucion" ("id_usuario");
