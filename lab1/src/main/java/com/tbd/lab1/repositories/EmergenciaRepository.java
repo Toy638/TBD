dd44d0a949c07280a91e0272758a4de0c477d3c4
RobertoGalleguillos <roberto.galleguillos@usach.cl>
2023-10-21 22:42:23 -0300

    Avances repositorios

---
+++ lab1/src/main/java/com/tbd/lab1/repositories/EmergenciaRepository.java
@@ -0,0 +1,19 @@
+package com.tbd.lab1.repositories;
+
+import com.tbd.lab1.entities.EmergenciaEntity;
+
+import java.util.List;
+
+
+public interface EmergenciaRepository {
+    public List<EmergenciaEntity> findAll();
+    public EmergenciaEntity create(EmergenciaEntity emergencia);
+    public EmergenciaEntity findById(Long id);
+    public EmergenciaEntity findByTarea(Long idTarea);
+    public EmergenciaEntity update(EmergenciaEntity emergencia);
+    public void delete(Long id);
+
+    void cambiarEstado(Long id);
+
+    int countTareasByEmergencia(Long id);
+}
