f68e03655d83f923eedcb6a5783a9a0c1382f7a6
AHaussG <andres.haussmann@usach.cl>
2023-10-22 21:50:43 -0300

    [Andrés Haussmann] - Cambios en script de triggers (.sql) y se agrega script de procedimiento (.sql)

--- triggersCreate.sql
+++ triggersCreate.sql
@@ -1,4 +1,4 @@
--- Tabla de auditoría para registrar las operaciones
+-- Tabla de auditoría para registrar las operaciones --
 CREATE TABLE audit_log (
   id serial PRIMARY KEY,
   table_name varchar(255),
@@ -8,7 +8,7 @@ CREATE TABLE audit_log (
   query text
 );
 
--- Función de auditoría
+-- Función de auditoría --
 CREATE OR REPLACE FUNCTION audit_trigger_function()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -18,53 +18,70 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- Triggers para las tablas a auditar
+
+-- Triggers para las tablas a auditar --
+----------------------------------------
+
+-- Trigger para tabla emergencia --
 CREATE TRIGGER audit_trigger_emergencia
 AFTER INSERT OR UPDATE OR DELETE ON emergencia
 FOR EACH ROW
 EXECUTE FUNCTION audit_trigger_function();
 
+-- Trigger para tabla tarea --
 CREATE TRIGGER audit_trigger_tarea
 AFTER INSERT OR UPDATE OR DELETE ON tarea
 FOR EACH ROW
 EXECUTE FUNCTION audit_trigger_function();
 
+-- Trigger para tabla habilidad --
 CREATE TRIGGER audit_trigger_habilidad
 AFTER INSERT OR UPDATE OR DELETE ON habilidad
 FOR EACH ROW
 EXECUTE FUNCTION audit_trigger_function();
 
+-- Trigger para tabla ranking --
 CREATE TRIGGER audit_trigger_ranking
 AFTER INSERT OR UPDATE OR DELETE ON ranking
 FOR EACH ROW
 EXECUTE FUNCTION audit_trigger_function();
 
+-- Trigger para tabla usuario --
 CREATE TRIGGER audit_trigger_usuario
 AFTER INSERT OR UPDATE OR DELETE ON usuario
 FOR EACH ROW
 EXECUTE FUNCTION audit_trigger_function();
 
+-- Trigger para tabla voluntario --
 CREATE TRIGGER audit_trigger_voluntario
 AFTER INSERT OR UPDATE OR DELETE ON voluntario
 FOR EACH ROW
 EXECUTE FUNCTION audit_trigger_function();
 
+-- Trigger para tabla voluntario_habilidad --
 CREATE TRIGGER audit_trigger_voluntario_habilidad
 AFTER INSERT OR UPDATE OR DELETE ON voluntario_habilidad
 FOR EACH ROW
 EXECUTE FUNCTION audit_trigger_function();
 
+-- Trigger para tabla tarea_habilidad --
 CREATE TRIGGER audit_trigger_tarea_habilidad
 AFTER INSERT OR UPDATE OR DELETE ON tarea_habilidad
 FOR EACH ROW
 EXECUTE FUNCTION audit_trigger_function();
 
+-- Trigger para tabla eme_habilidad --
 CREATE TRIGGER audit_trigger_eme_habilidad
 AFTER INSERT OR UPDATE OR DELETE ON eme_habilidad
 FOR EACH ROW
 EXECUTE FUNCTION audit_trigger_function();
 
+-- Trigger para tabla institucion --
 CREATE TRIGGER audit_trigger_institucion
 AFTER INSERT OR UPDATE OR DELETE ON institucion
 FOR EACH ROW
 EXECUTE FUNCTION audit_trigger_function();
+
+
+
+-- Testeo: SELECT * FROM audit_log;
