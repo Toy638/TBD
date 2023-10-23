c3fc237267d31176d9a373e6574da8d19691874e
AHaussG <andres.haussmann@usach.cl>
2023-10-22 23:07:49 -0300

    [Andrés Haussmann] - Arreglos en script de triggers (.sql)

--- triggersCreate.sql
+++ triggersCreate.sql
@@ -22,6 +22,30 @@ $$ LANGUAGE plpgsql;
 -- Triggers para las tablas a auditar --
 ----------------------------------------
 
+-- Trigger para tabla institucion --
+CREATE TRIGGER audit_trigger_institucion
+AFTER INSERT OR UPDATE OR DELETE ON institucion
+FOR EACH ROW
+EXECUTE FUNCTION audit_trigger_function();
+
+-- Trigger para tabla usuario --
+CREATE TRIGGER audit_trigger_usuario
+AFTER INSERT OR UPDATE OR DELETE ON usuario
+FOR EACH ROW
+EXECUTE FUNCTION audit_trigger_function();
+
+-- Trigger para tabla habilidad --
+CREATE TRIGGER audit_trigger_habilidad
+AFTER INSERT OR UPDATE OR DELETE ON habilidad
+FOR EACH ROW
+EXECUTE FUNCTION audit_trigger_function();
+
+-- Trigger para tabla voluntario --
+CREATE TRIGGER audit_trigger_voluntario
+AFTER INSERT OR UPDATE OR DELETE ON voluntario
+FOR EACH ROW
+EXECUTE FUNCTION audit_trigger_function();
+
 -- Trigger para tabla emergencia --
 CREATE TRIGGER audit_trigger_emergencia
 AFTER INSERT OR UPDATE OR DELETE ON emergencia
@@ -34,9 +58,9 @@ AFTER INSERT OR UPDATE OR DELETE ON tarea
 FOR EACH ROW
 EXECUTE FUNCTION audit_trigger_function();
 
--- Trigger para tabla habilidad --
-CREATE TRIGGER audit_trigger_habilidad
-AFTER INSERT OR UPDATE OR DELETE ON habilidad
+-- Trigger para tabla eme_habilidad --
+CREATE TRIGGER audit_trigger_eme_habilidad
+AFTER INSERT OR UPDATE OR DELETE ON eme_habilidad
 FOR EACH ROW
 EXECUTE FUNCTION audit_trigger_function();
 
@@ -46,39 +70,15 @@ AFTER INSERT OR UPDATE OR DELETE ON ranking
 FOR EACH ROW
 EXECUTE FUNCTION audit_trigger_function();
 
--- Trigger para tabla usuario --
-CREATE TRIGGER audit_trigger_usuario
-AFTER INSERT OR UPDATE OR DELETE ON usuario
-FOR EACH ROW
-EXECUTE FUNCTION audit_trigger_function();
-
--- Trigger para tabla voluntario --
-CREATE TRIGGER audit_trigger_voluntario
-AFTER INSERT OR UPDATE OR DELETE ON voluntario
-FOR EACH ROW
-EXECUTE FUNCTION audit_trigger_function();
-
--- Trigger para tabla voluntario_habilidad --
-CREATE TRIGGER audit_trigger_voluntario_habilidad
-AFTER INSERT OR UPDATE OR DELETE ON voluntario_habilidad
-FOR EACH ROW
-EXECUTE FUNCTION audit_trigger_function();
-
 -- Trigger para tabla tarea_habilidad --
 CREATE TRIGGER audit_trigger_tarea_habilidad
 AFTER INSERT OR UPDATE OR DELETE ON tarea_habilidad
 FOR EACH ROW
 EXECUTE FUNCTION audit_trigger_function();
 
--- Trigger para tabla eme_habilidad --
-CREATE TRIGGER audit_trigger_eme_habilidad
-AFTER INSERT OR UPDATE OR DELETE ON eme_habilidad
-FOR EACH ROW
-EXECUTE FUNCTION audit_trigger_function();
-
--- Trigger para tabla institucion --
-CREATE TRIGGER audit_trigger_institucion
-AFTER INSERT OR UPDATE OR DELETE ON institucion
+-- Trigger para tabla vol_habilidad --
+CREATE TRIGGER audit_trigger_vol_habilidad
+AFTER INSERT OR UPDATE OR DELETE ON vol_habilidad
 FOR EACH ROW
 EXECUTE FUNCTION audit_trigger_function();
 
